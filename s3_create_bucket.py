a8c03233a012fce7a597a1f067f5262825b25402
Kand <kandly2000@gmail.com>
2014-12-11 21:51:11 -0800

    Simplified region handling

--- s3_create_bucket.py
+++ s3_create_bucket.py
@@ -11,43 +11,41 @@ class S3Connection(object):
         self.connection = boto.connect_s3()
 
     def create_bucket(self, name, region):
-        if region == "":
-            self.connection.create_bucket(name)
-        else:
-            self.connection.create_bucket(name, None, region)
+        self.connection.create_bucket(name, None, region)
 
 
+def usage(progname):
+    print progname + ' -r <region> <bucket>'
+
+    
 def main(argv):
     bucket = ""
-    region = ""
+    region = Location.DEFAULT
 
     try:
         opts, args = getopt.getopt(argv[1:],"hr:")
     except getopt.GetoptError:
-        print 'Option error'
-        print argv[0] + ' -r <region> <bucket>'
+        usage(argv[0])
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'Usage:'
-            print argv[0] + ' -r <region> <bucket>'
+            usage(argv[0])
             print '\n'.join(eval('Location.' + i) for i in dir(Location) if i[0].isupper())
             sys.exit()
         elif opt == '-r':
-            region = str(arg)
+            region = arg
 
     if len(args) < 1:
-        print 'Arg error'
-        print argv[0] + ' -r <region> <bucket>'
+        usage(argv[0])
         sys.exit(2)
 
     bucket = args[0]
 
-    print 'Creating bucket ' + bucket + ' in ' + (region if region != "" else 'DEFAULT')
-        
     s3conn = S3Connection()
     s3conn.create_bucket(bucket, region)
 
+    print 'Created bucket ' + bucket + ' in ' + (region if region != "" else 'us-east-1')
+        
 if __name__ == "__main__":
     main(sys.argv)
